dbab44021c4918d2b1487ebaaed4b4dd6c156fdf
Kharka <adewoyindaniel6@gmail.com>
2024-03-12 01:46:40 +0000

    Ignore commit

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { SignUpPage } from '@pages';
-
-const App: React.FC = () => {
-	return <SignUpPage />;
-};
-
-export default App;
--- src/pages/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as HomePage } from './app/HomePage';
-export { default as SignInPage } from './auth/SignInPage';
-export { default as SignUpPage } from './auth/SignUpPage';
